db3a2600ff2322b6a8972613034a1d0e06a9047c
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-03-01 16:41:11 +0300

    Finished Task 6

--- task.js
+++ task.js
@@ -1,31 +1,38 @@
-const form = document.forms.formWithInput;
-const printResult = document.querySelector('.content__result');
-const inputNumber = document.querySelector('.form__input');
-const congratulation = document.querySelector('#congratulation');
+class Transport {
+  constructor(type, price, vendor, model) {
+    Transport.type = type;
+    Transport.price = price;
+    Transport.vendor = vendor;
+    Transport.model = model;
+  }
 
-function findNearestFactorial(value) {
-  if (value === '' || value === undefined) {
-    return '*';
+  getInfo() {
+    return `${Transport.vendor}, ${Transport.model}`;
   }
-  if (value <= 0) {
-    console.log(value);
-    return 1;
+
+  getPrice() {
+    return Transport.price.toLocaleString('ru-RU') + ' ₽';
   }
-  let currentValue = 1;
-  let count = 1;
-  for (let i = 1; currentValue * i <= value; i++) {
-    currentValue *= i;
-    count += 1;
-  }
-  return count - 1;
 }
 
-form.addEventListener('keyup', evt => {
-  evt.preventDefault();
-  printResult.textContent = findNearestFactorial(inputNumber.value);
-  congratulation.textContent = inputNumber.value && 'Вау, это успех!';
-});
+class Car extends Transport {
+  constructor(vendor, model, doorsCount, price) {
+    super('car', price, vendor, model);
+    Transport.doorsCount = doorsCount;
+  }
 
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-});
+  getDoorsCount() {
+    return `Кол-во дверей: ${Transport.doorsCount}`;
+  }
+}
+
+class Bike extends Transport {
+  constructor(vendor, model, maxSpeed, price) {
+    super('bike', price, vendor, model);
+    Transport.maxSpeed = maxSpeed;
+  }
+
+  getMaxSpeed() {
+    return `Макс. скорость: ${Transport.maxSpeed} км/ч`;
+  }
+}
