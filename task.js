ee69954102a23ec7c035eb70395e7d68a33f9b5a
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-03-01 16:17:51 +0300

    Finished Task 4

--- task.js
+++ task.js
@@ -1,10 +1,19 @@
-var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
+const numbers = [1, 2, 3, 4, 5, 6];
 
-getLastElementOfArray(words); // возвращает Свет;
+separateArray(numbers);
+// должен вернуть { even: [2, 4, 6], odd: [1, 3, 5] }
 
-function getLastElementOfArray(array) {
-  // Ваш код здесь
-  let wordLast = words[words.length - 1];
-
-  return wordLast;
+function separateArray(array) {
+// ваш код здесь
+    let sepNumbers = {};
+    sepNumbers.even = [];
+    sepNumbers.odd = [];
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        if (numbers[i] % 2 == 1) {
+            sepNumbers.odd.push(numbers[i]);
+        } else {
+            sepNumbers.even.push(numbers[i]);
+        }
+    }
+    return sepNumbers;
 }
