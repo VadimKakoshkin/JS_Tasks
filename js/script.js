7aa93fe1cf4ef6ebb953fd6323edda7799b46b39
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-03-01 16:10:48 +0300

    Finished Task 2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-const data = [
-  {name: "Саша", age: 19},
-  {name: "Катя", age: 21},
-  {name: "Миша", age: 17},
-  {name: "Федя", age: 23},
-  {name: "Клава", age: 22}
-];
-/*
-В переменной data хранится такой массив:
-[
-  {name: "Саша", age: 19},
-  {name: "Катя", age: 21},
-  {name: "Миша", age: 17},
-  {name: "Федя", age: 23},
-  {name: "Клава", age: 22}
-]
-*/
-calcAvgAge(data);
+const arr = [17, 23, 31, 44, 59];
 
-function calcAvgAge(array) {
-  // Напишите код здесь
-  let sum = 0;
-  for (const person of array) {
-    sum += person.age;
-  }
-  return Math.round(sum / array.length);
+let doubles = doubleNumber(arr); // возвращает [34,46,62,88,118];
+
+function doubleNumber(array) {
+  // Ваш код
+  return array.map(function(num) {
+    return num * 2;
+  });
+
 }
-
