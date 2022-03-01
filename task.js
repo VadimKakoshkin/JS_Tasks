f4d4a1500cb15d8ece8de658d368b7914056b7a3
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-03-01 16:24:14 +0300

    Finished Task 5

--- task.js
+++ task.js
@@ -1,19 +1,31 @@
-const numbers = [1, 2, 3, 4, 5, 6];
+const form = document.forms.formWithInput;
+const printResult = document.querySelector('.content__result');
+const inputNumber = document.querySelector('.form__input');
+const congratulation = document.querySelector('#congratulation');
 
-separateArray(numbers);
-// должен вернуть { even: [2, 4, 6], odd: [1, 3, 5] }
-
-function separateArray(array) {
-// ваш код здесь
-    let sepNumbers = {};
-    sepNumbers.even = [];
-    sepNumbers.odd = [];
-    for (let i = 0; i <= numbers.length - 1; i++) {
-        if (numbers[i] % 2 == 1) {
-            sepNumbers.odd.push(numbers[i]);
-        } else {
-            sepNumbers.even.push(numbers[i]);
-        }
-    }
-    return sepNumbers;
+function findNearestFactorial(value) {
+  if (value === '' || value === undefined) {
+    return '*';
+  }
+  if (value <= 0) {
+    console.log(value);
+    return 1;
+  }
+  let currentValue = 1;
+  let count = 1;
+  for (let i = 1; currentValue * i <= value; i++) {
+    currentValue *= i;
+    count += 1;
+  }
+  return count - 1;
 }
+
+form.addEventListener('keyup', evt => {
+  evt.preventDefault();
+  printResult.textContent = findNearestFactorial(inputNumber.value);
+  congratulation.textContent = inputNumber.value && 'Вау, это успех!';
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+});
